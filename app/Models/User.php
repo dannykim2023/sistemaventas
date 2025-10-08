4c96035b3caa85e4fd7cdda79133a111d20d5a5d
Daniel <dannykim2023@gmail.com>
2025-10-08 01:33:57 -0500

    Descripción clara del cambio realizado

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Los atributos que se pueden asignar en masa.
      *
      * @var list<string>
      */
@@ -24,7 +24,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * Los atributos que deben ocultarse para la serialización.
      *
      * @var list<string>
      */
@@ -34,7 +34,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * Los atributos que deben ser casteados.
      *
      * @return array<string, string>
      */
@@ -44,5 +44,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Determina si el usuario puede acceder al panel de Filament.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Durante pruebas, deja esto en true:
+        return true;
+
+        // 🔒 En producción podrías hacer algo así:
+        // return $this->email === 'admin@tudominio.com';
     }
 }
